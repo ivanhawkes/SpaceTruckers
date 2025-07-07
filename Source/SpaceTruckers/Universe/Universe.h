9649cc115c52f79b6f8c6286e5ff2c924b708e00
Ivan Hawkes <ivan.hawkes@gmail.com>
2025-07-07 18:17:12 +1000

    Added seed to the universe, made it a data thingy.

--- Source/SpaceTruckers/Universe/Universe.h
+++ Source/SpaceTruckers/Universe/Universe.h
@@ -1,18 +1,24 @@
 #pragma once
 
+#include "Engine/DataTable.h"
 #include "Universe.generated.h"
 
 
-USTRUCT(BlueprintType)
-struct FUniverse
+UCLASS(BlueprintType, Blueprintable)
+class SPACETRUCKERS_API UUniverse : public UDataAsset
 {
     GENERATED_BODY()
 
 public:
-    UPROPERTY(BlueprintReadWrite)
+    UPROPERTY(EditAnywhere, Category = "Item Data")
+    FName ID;
+
+    UPROPERTY(EditAnywhere, Category = "Item Data")
+    uint64 seed;
+
+    UPROPERTY(EditAnywhere, Category = "Item Data")
     FText UniverseName {FText::FromString("Worst Timeline")};
 
-    UPROPERTY(BlueprintReadWrite)
+    UPROPERTY(EditAnywhere, Category = "Item Data")
     FVector2D Location {0.0f, 0.0f};
-
 };
